0a56fbe0ae37d6456ac228ba4f645979e01f87c2
SBTBillionz <sbtflex33@gmail.com>
2025-07-14 13:42:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -92,4 +92,4 @@ app.post("/api/loginDoctor", (req, res) => {
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //   console.log(`🚀 Server running on port ${PORT}`);
-// });
+// }); 
